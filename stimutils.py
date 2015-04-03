8a7b0503f30ca1c261c03f69a50ed0532c3dd573
mmagnuski <mmagnuski@swps.edu.pl>
2015-04-04 01:01:07 +0200

    move stimuli creation to stimutils

---
+++ stimutils.py
@@ -0,0 +1,68 @@
+from psychopy import visual
+from exputils import getFrameRate
+import numpy  as np
+
+
+# create a window
+# ---------------
+win = visual.Window([800,600],monitor="testMonitor", 
+    units="deg", color = [-0.2, -0.2, -0.2], fullscr=True)
+# get fame rate
+frm = getFrameRate(win)
+win.setMouseVisible(False)
+
+
+# ==def for gabor creation==
+def gabor(win = win, ori = 0, opa = 1.0, 
+		  pos  = [0, 0], size = 7, 
+		  units = 'deg', sf = 1.5):
+	return visual.GratingStim(win     = win,  mask = "gauss", \
+							  size    = size, pos  = pos, \
+							  sf      = sf,   ori  = ori,      \
+							  opacity = opa,  units = units)
+
+def whiteshape(v, win = win):
+	return visual.ShapeStim(
+		win, 
+		lineWidth  = 0.5, 
+		fillColor  = [1, 1, 1], 
+		lineColor  = [1, 1, 1], 
+		vertices   = v, 
+		closeShape = True
+		)
+
+# create fixation cross:
+def fix():
+	v = np.array(
+			[
+				[0.1, -1], 
+				[0.1, 1],
+				[-0.1, 1],
+				[-0.1, -1]
+			]
+		)
+
+	fix = []
+	fix.append(whiteshape(v))
+	fix.append(whiteshape(
+		np.fliplr(v)
+		))
+	return fix
+
+# prepare stimuli
+stim = {}
+stim['window'] = win
+stim['target'] = gabor()
+# CHANGE - window size, so that it is accurate...
+stim['centerImage'] = visual.ImageStim(win, image=None,  
+            pos=(0.0, 0.0), size=(14*80,6*80), units = 'pix')
+
+
+# mask - all gabor directions superposed
+mask_ori = [0, 45, 90, 135]
+stim['mask'] = []
+
+for o in mask_ori:
+	stim['mask'].append(gabor(ori = o, opa = 0.25))
+
+stim['fix'] = fix()
--- test_gabor_weib.py
+++ test_gabor_weib.py
@@ -77,13 +77,6 @@ portdict['codes'].update({'target_'+str(ori) : 4+i \
 print 'keymap: ', exp['keymap']
 exp['participant'] = getUserName(intUser = False)
 
-# create a window
-# ---------------
-win = visual.Window([800,600],monitor="testMonitor", 
-    units="deg", color = [-0.2, -0.2, -0.2], fullscr=True)
-# get fame rate
-frm = getFrameRate(win)
-win.setMouseVisible(False)
 
 # get path
 pth   = os.path.dirname(os.path.abspath(__file__))
@@ -146,58 +139,6 @@ else:
 	exp['numTrials'] = len(db)
 
 
-# ==def for gabor creation==
-def gabor(win = win, ori = 0, opa = 1.0, 
-		  pos  = [0, 0], size = 7, 
-		  units = 'deg', sf = 1.5):
-	return visual.GratingStim(win     = win,  mask = "gauss", \
-							  size    = size, pos  = pos, \
-							  sf      = sf,   ori  = ori,      \
-							  opacity = opa,  units = units)
-
-# prepare stimuli
-stim = {}
-stim['window'] = win
-stim['target'] = gabor()
-# CHANGE - window size, so that it is accurate...
-stim['centerImage'] = visual.ImageStim(win, image=None,  
-            pos=(0.0, 0.0), size=(14*80,6*80), units = 'pix')
-
-
-# mask - all gabor directions superposed
-mask_ori = [0, 45, 90, 135]
-stim['mask'] = []
-
-for o in mask_ori:
-	stim['mask'].append(gabor(ori = o, opa = 0.25))
-
-# create fixation cross:
-v = np.array(
-		[
-			[0.1, -1], 
-			[0.1, 1],
-			[-0.1, 1],
-			[-0.1, -1]
-		]
-	)
-
-def whiteshape(v, win = win):
-	return visual.ShapeStim(
-		win, 
-		lineWidth  = 0.5, 
-		fillColor  = [1, 1, 1], 
-		lineColor  = [1, 1, 1], 
-		vertices   = v, 
-		closeShape = True
-		)
-
-fix = []
-fix.append(whiteshape(v))
-fix.append(whiteshape(
-	np.fliplr(v)
-	))
-stim['fix'] = fix
-del fix
 
 def onflip_work(portdict, code='', clock=None):
 	if clock:
