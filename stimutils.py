6d22b02846525d1165158a26c36910c0b833567a
mmagnuski <mmagnuski@swps.edu.pl>
2015-04-09 14:20:59 +0200

    move sim pres defs (etc) to stimutils

--- stimutils.py
+++ stimutils.py
@@ -1,5 +1,7 @@
 from psychopy import visual
-from exputils import getFrameRate
+from ctypes    import windll
+from settings import exp, db, startTrial
+from exputils  import getFrameRate
 import numpy  as np
 
 
@@ -8,6 +10,8 @@ import numpy  as np
 win = visual.Window([800,600],monitor="testMonitor", 
     units="deg", color = [-0.2, -0.2, -0.2], fullscr=True)
 win.setMouseVisible(False)
+# get frame rate:
+exp['frm'] = getFrameRate(stim['window'])
 
 
 # ==def for gabor creation==
@@ -64,3 +68,157 @@ for o in mask_ori:
 	stim['mask'].append(gabor(ori = o, opa = 0.25))
 
 stim['fix'] = fix()
+
+
+# stimuli presentation etc.
+# -------------------------
+
+# def for onflip clock reset and port trigger
+def onflip_work(portdict, code='', clock=None):
+	if clock:
+		clock.reset()
+	if portdict['send'] and code:
+		windll.inpout32.Out32(portdict['port address'], 
+			portdict['codes'][code])
+
+
+# we do not need to clear port on the new amplifier
+# so this short def is 'just-in-case'
+def clear_port(portdict):
+	windll.inpout32.Out32(portdict['port address'], 0)
+
+
+def present_trial(tr, exp = exp, stim = stim, db = db, 
+					  win = stim['window']):
+	# PREPARE
+	# -------
+	db.loc[tr, 'opacity'] = np.round(
+		np.random.uniform(
+			low = exp['opacity'][0], 
+			high = exp['opacity'][1], 
+			size = 1
+			)[0], decimals = 3)
+	# set target properties (takes less than 1 ms)
+	stim['target'].ori = db.loc[tr]['orientation']
+	stim['target'].opacity = db.loc[tr]['opacity']
+	target_code = 'target_' + str(db.loc[tr]['orientation'])
+
+	# get trial start time (!)
+	db.loc[tr, 'time'] = core.getTime()
+
+	# present fix:
+	win.callOnFlip(onflip_work, portdict, code='fix')
+	for f in np.arange(db.loc[tr]['fixTime']):
+		for fx in stim['fix']:
+			fx.draw()
+		win.flip()
+
+	# present target
+	win.callOnFlip(onflip_work, portdict, code=target_code,
+		clock=exp['clock'])
+	for f in np.arange(db.loc[tr]['targetTime']):
+		stim['target'].draw()
+		win.flip()
+
+	# interval
+	for f in np.arange(db.loc[tr]['SMI']):
+		win.flip()
+
+	# mask
+	win.callOnFlip(onflip_work, portdict, code='mask')
+	for f in np.arange(db.loc[tr]['maskTime']):
+		for m in stim['mask']:
+			m.draw()
+		win.flip()
+
+	# RESPONSE
+	# --------
+	# check if response
+	k = event.getKeys(keyList = exp['use keys'] + ['q'], 
+					  timeStamped = exp['clock'])
+	
+	# wait for response (timeout)
+	if not k:
+		k = event.waitKeys(maxWait = exp['respWait'], 
+					       keyList = exp['use keys'] + ['q'], 
+					       timeStamped = exp['clock'])
+	
+	# calculate RT and ifcorrect
+	if k:
+		key, RT = k[0]
+
+		# if debug - test for quit
+		if exp['debug'] and key == 'q':
+				core.quit()
+
+		# performance
+		db.loc[tr, 'response']  = key
+		db.loc[tr, 'RT']        = RT
+		db.loc[tr, 'ifcorrect'] = int(exp['keymap']\
+									  [db.loc[tr]['orientation']] == key)
+	else:
+		db.loc[tr, 'response']  = 'NoResp'
+		db.loc[tr, 'ifcorrect'] = 0
+
+
+# instructions etc.
+# -----------------
+
+# this should go to instructions module
+def show_resp_rule(exp = exp, win = stim['window']):
+
+	# create diagonal on one side and cardinal on the other
+	ch    = exp['choose_resp']
+
+	stims = []
+	ornt      = [[0, 90], [45, 135]]
+	ornt      = ornt[ch] + ornt[1- ch]
+
+	positions = [[-1, -0.5], [-1, 0.5], \
+				 [1, -0.5],  [1, 0.5]]
+	positions = np.array(positions) * 10
+
+	for o, p in zip(ornt, positions):
+		stims.append(gabor(ori = o, pos  = p, size = 4, 
+						   units = 'deg'))
+
+	# add info on correct responses
+	tx = [
+		u'naciśnij  ' + exp['keymap'][ornt[0]], 
+		u'naciśnij  ' + exp['keymap'][ornt[2]],
+		u'aby przejść dalej naciśnij spację'
+		]
+	positions = [
+		[-0.5, 0.75], 
+		[0.5, 0.75],
+		[0, -0.85]
+		]
+	txStim = []
+
+	for t, p in zip(tx, positions):
+		txStim.append(visual.TextStim(win, text = t, pos = p, units = 'norm'))
+
+	# draw all:
+	for t in txStim:
+		t.draw()
+
+	for g in stims:
+		g.draw()
+
+	win.flip()
+
+	# wait for space:
+	k = event.waitKeys(keyList = ['space'])
+
+
+def present_break(t, exp = exp, win = stim['window']):
+	tex  = u'Ukończono  {0} / {1}  powtórzeń.\nMożesz teraz ' + \
+		   u'chwilę odetchnąc.\nNaciśnij spację aby kontynuowac...'
+	tex  = tex.format(t, exp['numTrials'])
+	info = visual.TextStim(win, text = tex, pos = [0, 0], units = 'norm')
+	
+	info.draw()
+	win.flip()
+
+	# wait for space key:
+	k = event.waitKeys(keyList = ['space', 'escape', 'return'])
--- test_gabor_weib.py
+++ test_gabor_weib.py
@@ -27,283 +27,16 @@
 # imports
 print 'importing psychopy...'
 from psychopy  import visual, core, event
-from exputils  import getFrameRate, ms2frames, getUserName, continue_dataframe, Weibull, plot_Feedback
+from exputils  import Weibull, plot_Feedback
 from random    import randint, uniform #, choice
-from ctypes    import windll
+from stimutlis import exp, db, startTrial, present_trial, \
+					  present_break, show_resp_rules
 import os
 import numpy  as np
 import pandas as pd
 
 
-# experiment settings
-# -------------------
-exp = {}
-exp['debug']       = True
-exp['clock']       = core.Clock()
-exp['use trigger'] = True
-exp['port address'] = '0xDC00' # string, for example '0xD05'
-exp['break after'] = 15 # how often subjects have a break
-
-exp['participant'] = getUserName(intUser = False)
-
-exp['targetTime']  = [1]
-exp['SMI']         = [2] # Stimulus Mask Interval
-exp['fixTimeLim']  = [0.75, 2.5]
-exp['maskTime']    = [20]
-exp['opacity']     = [0.05, 0.8]
-exp['orientation'] = [0, 45, 90, 135]
-exp['corrLims']    = [0.55, 0.9]
-
-exp['use keys']    = ['f', 'j']
-exp['respWait']    = 1.5
-
-# response mapping
-# ----------------
-resp = {}
-choose_resp = randint(0, 1)
-resp[0]   = exp['use keys'][choose_resp]
-resp[90]  = exp['use keys'][choose_resp]
-resp[45]  = exp['use keys'][1 - choose_resp]
-resp[135] = exp['use keys'][1 - choose_resp]
-exp['keymap'] = resp
-exp['choose_resp'] = choose_resp
-
-# port settings
-# -------------
-portdict = {}
-portdict['send'] = exp['use trigger']
-portdict['port address'] = int(exp['port address'], base=16) \
-						   if exp['port address'] and portdict['send'] \
-						   else exp['port address']
-portdict['codes'] = {'fix' : 1, 'mask' : 2}
-portdict['codes'].update({'target_'+str(ori) : 4+i \
-						   for i,ori in enumerate(exp['orientation'])
-						   })
-
-
-# change below to logging:
-print 'keymap: ', exp['keymap']
-
-
-# get path
-pth   = os.path.dirname(os.path.abspath(__file__))
-exp['path'] = pth
-# ensure 'data' directory is available:
-exp['data'] = os.path.join(pth, 'data')
-if not os.path.isdir(exp['data']):
-	os.mkdir(exp['data'])
-
-# get frame rate:
-from stimutils import stim, gabor
-frm = getFrameRate(stim['window'])
-
-# check if continue with previous dataframe:
-ifcnt = continue_dataframe(exp['data'], exp['participant'] + '.xls')
-
-if not ifcnt:
-	# create DataFrame
-	# ----------------
-	# define column names:
-	colNames = ['time', 'fixTime', 'targetTime', 'SMI', \
-				'maskTime', 'opacity', 'orientation', \
-				'response', 'ifcorrect', 'RT']
-	# CHANGE - dtypes are not being used
-	dtypes   = ['float', 'float', 'int', 'int', \
-				'int', 'float', 'int', 'str', 'int', 'float']
-	# combined columns
-	cmb = ['targetTime', 'SMI', 'maskTime', 'orientation']
-	# how many times each combination should be presented
-	perComb = 140;
-	# generate trial combinations
-	lst = [
-			[i, j, l, m ] \
-				for i in exp['targetTime']  \
-				for j in exp['SMI']         \
-				for l in exp['maskTime']    \
-				for m in exp['orientation'] \
-			if i + j < 5] \
-		* perComb
-
-	# turn to array and shuffle rows
-	lst = np.array(lst)
-	np.random.shuffle(lst)
-
-	# construct data frame
-	shp = lst.shape
-	db = pd.DataFrame(
-		index = np.arange(1, shp[0] + 1),
-		columns = colNames
-		)
-	exp['numTrials'] = len(db)
-
-	# fill the data frame from lst
-	for i, r in enumerate(cmb):
-		db[r] = lst[:, i]
-
-	# add fix time in frames
-	db['fixTime'] = ms2frames( 
-		np.random.uniform(
-			low = exp['fixTimeLim'][0], 
-			high = exp['fixTimeLim'][1], 
-			size = exp['numTrials']
-			) * 1000,
-		frm['time']
-		)
-
-	startTrial = 1
-else:
-	db, startTrial = ifcnt
-	exp['numTrials'] = len(db)
-
-
-
-def onflip_work(portdict, code='', clock=None):
-	if clock:
-		clock.reset()
-	if portdict['send'] and code:
-		windll.inpout32.Out32(portdict['port address'], 
-			portdict['codes'][code])
-
-
-# we do not need to clear port on the new amplifier
-# so this short def is 'just-in-case'
-def clear_port(portdict):
-	windll.inpout32.Out32(portdict['port address'], 0)
-
-
-def present_trial(tr, exp = exp, stim = stim, db = db, 
-					  win = stim['window']):
-	# PREPARE
-	# -------
-	db.loc[tr, 'opacity'] = np.round(
-		np.random.uniform(
-			low = exp['opacity'][0], 
-			high = exp['opacity'][1], 
-			size = 1
-			)[0], decimals = 3)
-	# set target properties (takes less than 1 ms)
-	stim['target'].ori = db.loc[tr]['orientation']
-	stim['target'].opacity = db.loc[tr]['opacity']
-	target_code = 'target_' + str(db.loc[tr]['orientation'])
-
-	# get trial start time (!)
-	db.loc[tr, 'time'] = core.getTime()
-
-	# present fix:
-	win.callOnFlip(onflip_work, portdict, code='fix')
-	for f in np.arange(db.loc[tr]['fixTime']):
-		for fx in stim['fix']:
-			fx.draw()
-		win.flip()
-
-	# present target
-	win.callOnFlip(onflip_work, portdict, code=target_code,
-		clock=exp['clock'])
-	for f in np.arange(db.loc[tr]['targetTime']):
-		stim['target'].draw()
-		win.flip()
-
-	# interval
-	for f in np.arange(db.loc[tr]['SMI']):
-		win.flip()
-
-	# mask
-	win.callOnFlip(onflip_work, portdict, code='mask')
-	for f in np.arange(db.loc[tr]['maskTime']):
-		for m in stim['mask']:
-			m.draw()
-		win.flip()
-
-	# RESPONSE
-	# --------
-	# check if response
-	k = event.getKeys(keyList = exp['use keys'] + ['q'], 
-					  timeStamped = exp['clock'])
-	
-	# wait for response (timeout)
-	if not k:
-		k = event.waitKeys(maxWait = exp['respWait'], 
-					       keyList = exp['use keys'] + ['q'], 
-					       timeStamped = exp['clock'])
-	
-	# calculate RT and ifcorrect
-	if k:
-		key, RT = k[0]
-
-		# if debug - test for quit
-		if exp['debug'] and key == 'q':
-				core.quit()
-
-		# performance
-		db.loc[tr, 'response']  = key
-		db.loc[tr, 'RT']        = RT
-		db.loc[tr, 'ifcorrect'] = int(exp['keymap']\
-									  [db.loc[tr]['orientation']] == key)
-	else:
-		db.loc[tr, 'response']  = 'NoResp'
-		db.loc[tr, 'ifcorrect'] = 0
-
-
-# this should go to instructions module
-def show_resp_rules(exp = exp, win = stim['window']):
-
-	# create diagonal on one side and cardinal on the other
-	ch    = exp['choose_resp']
-
-	stims = []
-	ornt      = [[0, 90], [45, 135]]
-	ornt      = ornt[ch] + ornt[1- ch]
-
-	positions = [[-1, -0.5], [-1, 0.5], \
-				 [1, -0.5],  [1, 0.5]]
-	positions = np.array(positions) * 10
-
-	for o, p in zip(ornt, positions):
-		stims.append(gabor(ori = o, pos  = p, size = 4, 
-						   units = 'deg'))
-
-	# add info on correct responses
-	tx = [
-		u'naciśnij  ' + exp['keymap'][ornt[0]], 
-		u'naciśnij  ' + exp['keymap'][ornt[2]],
-		u'aby przejść dalej naciśnij spację'
-		]
-	positions = [
-		[-0.5, 0.75], 
-		[0.5, 0.75],
-		[0, -0.85]
-		]
-	txStim = []
-
-	for t, p in zip(tx, positions):
-		txStim.append(visual.TextStim(win, text = t, pos = p, units = 'norm'))
-
-	# draw all:
-	for t in txStim:
-		t.draw()
-
-	for g in stims:
-		g.draw()
-
-	win.flip()
-
-	# wait for space:
-	k = event.waitKeys(keyList = ['space'])
-
-
-def present_break(t, exp = exp, win = stim['window']):
-	tex  = u'Ukończono  {0} / {1}  powtórzeń.\nMożesz teraz ' + \
-		   u'chwilę odetchnąc.\nNaciśnij spację aby kontynuowac...'
-	tex  = tex.format(t, exp['numTrials'])
-	info = visual.TextStim(win, text = tex, pos = [0, 0], units = 'norm')
-	
-	info.draw()
-	win.flip()
-
-	# wait for space key:
-	k = event.waitKeys(keyList = ['space', 'escape', 'return'])
-
-
+# some remaining defs:
 def set_opacity_if_fit_fails(corr, exp):
 	mean_corr = np.mean(corr)
 	if mean_corr > 0.8:
@@ -330,10 +63,16 @@ def fit_weibull(db, i):
 	w.fit([1.0, 1.0])
 	return w
 
+
+# start experiment
+# ----------------
+
+# INSTRUCTIONS!
+
 # show response rules:
 show_resp_rules()
 
-# show trials:
+# trial loop:
 for i in range(startTrial, exp['numTrials'] + 1):
 	present_trial(i)
 	stim['window'].flip()
