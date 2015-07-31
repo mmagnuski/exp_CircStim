186aa44ceb86dfe8e1c9c2ff626a658c605acefd
mmagnuski <mmagnuski@swps.edu.pl>
2015-07-31 12:21:22 +0200

    adds simple test for ContrastInterface

---
+++ test_interf.py
@@ -0,0 +1,14 @@
+from psychopy import visual, event, core
+from exputils import ContrastInterface
+
+stim = dict()
+stim['window'] = visual.Window(monitor='testMonitor')
+stim['centerImage'] = visual.ImageStim(stim['window'], image=r'data\T004_weibull_fit_temp.png')
+
+interf = ContrastInterface(stim=stim)
+
+while True:
+	interf.refresh()
+	k = event.getKeys()
+	if k and 'q' in k:
+		core.quit()
