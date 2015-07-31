0eecfe07de7f1bcd11e358db7a423b00302f8da9
mmagnuski <mmagnuski@swps.edu.pl>
2015-07-31 14:33:05 +0200

    update test_interf.py

--- test_interf.py
+++ test_interf.py
@@ -2,7 +2,7 @@ from psychopy import visual, event, core
 from exputils import ContrastInterface
 
 stim = dict()
-stim['window'] = visual.Window(monitor='testMonitor')
+stim['window'] = visual.Window(monitor='testMonitor', fullscr=True)
 stim['centerImage'] = visual.ImageStim(stim['window'], image=r'data\T004_weibull_fit_temp.png')
 
 interf = ContrastInterface(stim=stim)
