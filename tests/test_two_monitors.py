96f63ff9accf056357fd7a709881f77592449405
mmagnuski <mmagnuski@swps.edu.pl>
2015-08-06 20:52:35 +0200

    fix test for two screens

--- tests/test_two_monitors.py
+++ tests/test_two_monitors.py
@@ -2,6 +2,7 @@
 import time
 from psychopy.visual import Window
 from psychopy.visual import TextStim
+from psychopy import event
 
 # create a Window
 create_win = lambda s: Window(size=(1280,1024), fullscr=True,
@@ -12,6 +13,12 @@ win2 = create_win(1)
 txt = list()
 txt.append(TextStim(win1, text="This is screen 1"))
 txt.append(TextStim(win2, text="This is screen 2"))
+txt2 = TextStim(win2, pos=(0, -0.5), text="another text")
 [t.draw() for t in txt]
 win1.flip()
-win2.flip()
+event.waitKeys()
+win2.flip(clearBuffer=False)
+event.waitKeys()
+txt2.draw()
+win2.flip(clearBuffer=False)
+event.waitKeys()
