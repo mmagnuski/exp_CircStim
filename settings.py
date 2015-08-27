64540fd99725a5ee5a374c22193e3b675291d33c
mmagnuski <mmagnuski@swps.edu.pl>
2015-08-27 15:33:41 +0200

    no need to monkey patch pyglet shaders everywhere

--- settings.py
+++ settings.py
@@ -1,21 +1,3 @@
-
-# monkey-patch pyglet shaders:
-# ----------------------------
-fragFBOtoFramePatched = '''
-    uniform sampler2D texture;
-
-    float rand(vec2 seed){
-        return fract(sin(dot(seed.xy ,vec2(12.9898,78.233))) * 43758.5453);
-    }
-
-    void main() {
-        vec4 textureFrag = texture2D(texture,gl_TexCoord[0].st);
-        gl_FragColor.rgb = textureFrag.rgb;
-    }
-    '''
-from psychopy import _shadersPyglet
-_shadersPyglet.fragFBOtoFrame = fragFBOtoFramePatched
-
 # imports
 import os
 import numpy  as np
