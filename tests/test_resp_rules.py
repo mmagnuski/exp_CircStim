e12b850c53f2c31286e8b2110358569b0b35bcd8
mmagnuski <mmagnuski@swps.edu.pl>
2016-02-12 12:55:13 +0100

    testing resp_rules

---
+++ tests/test_resp_rules.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from psychopy  import visual, core
+from stimutils import exp, db, stim, show_resp_rules
+
+show_resp_rules(exp=exp, text=u"Tak wygląda ekran przerwy.")
+core.quit()
