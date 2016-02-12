0ad377690b672c3b70fba996e015271ef8c0693f
mmagnuski <mmagnuski@swps.edu.pl>
2016-02-12 12:54:54 +0100

    change tests a little

--- test_resp_rules.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from psychopy  import visual, core
-from stimutils import exp, db, stim, show_resp_rules
-
-show_resp_rules(exp=exp, text=u"Tak wygląda ekran przerwy.")
-core.quit()
--- tests/test_interf.py
+++ tests/test_interf.py
@@ -3,12 +3,8 @@ from exputils import ContrastInterface
 
 stim = dict()
 stim['window'] = visual.Window(monitor='testMonitor', fullscr=True)
-stim['centerImage'] = visual.ImageStim(stim['window'], image=r'data\T004_weibull_fit_temp.png')
+stim['centerImage'] = visual.ImageStim(stim['window'],
+    image=r'data\example_weibull_fit.png', units='pix')
 
 interf = ContrastInterface(stim=stim)
-
-while True:
-	interf.refresh()
-	k = event.getKeys()
-	if k and 'q' in k:
-		core.quit()
+interf.loop()
