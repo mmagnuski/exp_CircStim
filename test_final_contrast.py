0d6edab529b0240632722f8598cab3f1d0a443e7
mmagnuski <mmagnuski@swps.edu.pl>
2016-09-14 18:18:30 +0200

    make tests run on my laptop :)

--- test_final_contrast.py
+++ test_final_contrast.py
@@ -10,11 +10,13 @@ from weibull import fitw
 
 
 # read data
-pth = exp['data']
+# pth = exp['data']
 # fl = 'new_test_01_b_1.xls'
 # fl = 'Alexis01_b.xls'
 # fl = 'T19_b_1.xls'
-fl = 'testing_miko_01_b_1.xls'
+# fl = 'testing_miko_01_b_1.xls'
+pth = r'C:\proj\src\granprel\data\beh\pretest 02'
+fl = 'pretest_02_001b_b_1.xls'
 flpth = os.path.join(pth, fl)
 df = pd.read_excel(flpth)
 
--- test_interf.py
+++ test_interf.py
@@ -6,14 +6,11 @@ from exputils import ContrastInterface
 
 # edit stim
 stim['centerImage'] = visual.ImageStim(stim['window'],
-    image=r'data\weibull_fit_temp.png', units='pix')
+    image=r'C:\proj\src\granprel\data\beh\pretest 01\T004_weibull_fit_temp.png', units='pix')
 
 # read data
-pth = exp['data']
-# fl = 'new_test_01_b_1.xls'
-# fl = 'Alexis01_b.xls'
-# fl = 'T19_b_1.xls'
-fl = 'testing_miko_01_b_1.xls'
+pth = r'C:\proj\src\granprel\data\beh\pretest 02'
+fl = 'pretest_02_001b_b_1.xls'
 flpth = os.path.join(pth, fl)
 df = pd.read_excel(flpth)
 
